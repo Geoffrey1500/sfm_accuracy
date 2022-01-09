726185f6593ad35ea0f739ae30f1b45b4a9217cb
Hong <Geoffrey.1500@outlook.com>
2022-01-10 01:44:39 +0800

    无可视化的版本基本完成，下一步需要考虑处理异常，例如和参考点云无交集的情况

--- final_version_without_visulization.py
+++ final_version_without_visulization.py
@@ -101,13 +101,8 @@ Step 3: gaussian weighted euclidean distance calc
 
 rot_mat_set = R.from_euler('ZXY', euler_ang, degrees=True)
 
-mesh_tower = pv.read("data/1_8_2.ply")
-mesh_tower.scale([1000, 1000, 1000])
-
-pcd = o3d.io.read_point_cloud("data/1_8_2.ply")
-
 mesh_for_trimesh = trimesh.load("data/1_8_2.glb", force='mesh')
-trimesh_points = mesh_for_trimesh.vertices*1000
+points_coor = np.asarray(mesh_for_trimesh.vertices)*1000
 trimesh_triangle = mesh_for_trimesh.triangles
 vertices_normal = mesh_for_trimesh.vertex_normals
 
@@ -120,21 +115,17 @@ end_time = time.process_time()
 print("读取参考点云数据总耗时 ：" + str(end_time - start_time) + "s")
 points_in_ref = np.asarray(pcd_ref.points) * 1000
 
+density_ratio = int(len(points_in_ref)/len(points_coor))
+
 start_time = time.process_time()
 kdt = grab_tree("tree.txt")
 end_time = time.process_time()
 print("读取knn树总耗时 ：" + str(end_time - start_time) + "s")
 
-density_ratio = int(len(points_in_ref)/len(trimesh_points))
-
 vertices_normal_after = mesh_for_trimesh.vertex_normals
 
-points_coor = np.asarray(pcd.points)*1000
 
 for j in np.arange(4068, 4078):
-    plotter = pv.Plotter()
-    plotter.add_mesh(mesh_tower, show_edges=True, color="white")
-
     start = points_coor[j]
     start_vertex_normals = mesh_for_trimesh.vertex_normals[j]
     coneA = 0.00001
@@ -167,16 +158,8 @@ for j in np.arange(4068, 4078):
             dis_check = tri_queried[0] - points_wanted
             sum_dis_check = np.sum(dis_check**2, axis=1)
 
-            # 加入射线与目标点法向量的限制，过滤掉夹角大于60度的射线
+            # 加入射线与目标点法向量的限制，过滤掉夹角大于30度的射线
             if np.any(sum_dis_check <= 0.0001) and angle_between_vectors(start_vertex_normals, (stop - start) / 1000) > 0.866:
-                sphere = pv.Sphere(radius=1000, center=cam_loc[i])
-                ray = pv.Line(start, stop)
-                intersection = pv.PolyData(point_cam)
-                plotter.add_mesh(sphere, color="red", opacity=1)
-                plotter.add_mesh(ray, color="green", line_width=1, label="Ray Segment", opacity=1)
-                plotter.add_mesh(intersection, color="blue",
-                                 point_size=15, label="Intersection Points")
-                plotter.add_mesh(sensor_plane, show_edges=False, opacity=1, color="green")
 
                 # 初始化第一个圆锥，并暂时跳出循环
                 if coneA == 0.00001:
@@ -197,23 +180,11 @@ for j in np.arange(4068, 4078):
 
     end_time = time.process_time()
 
-    _ = plotter.add_axes(box=True)
-
     if v != 0:
-        points = np.asarray(meshA.vertices)
-        faces = np.asarray(meshA.faces)
-
-        print(len(points))
-        # print(faces)
         print("一共模拟 " + str(v) + " 个点")
         print("总共有 " + str(v/len(cam_loc)*100) + "% 的相机参与运算")
         print("相交体积为：" + str(meshA.volume) + "mm^3")
         print("共运行：" + str(end_time - start_time) + "s")
-
-        # faces = [[0, 1, 2]]
-        mesh = pv.make_tri_mesh(points, faces)
-        # mesh = pyvista.wrap(tmesh)
-        mesh.plot(show_edges=True, line_width=1)
 
         core_point = points_coor[j].reshape((1, 3))
         dis_tree, idx = kdt.query(core_point/1000, k=density_ratio, return_distance=True)
@@ -227,28 +198,12 @@ for j in np.arange(4068, 4078):
 
         intersection_mesh = meshA.as_open3d
 
-        core_from_target = o3d.geometry.TriangleMesh.create_sphere(radius=2.0).translate((core_point[0, 0], core_point[0, 1], core_point[0, 2]))
-        o3d.visualization.draw_geometries([pcd_neighbor_set, core_from_target, intersection_mesh],
-                                          mesh_show_wireframe=True,
-                                          window_name='2 pixel')
-
-        o3d.visualization.draw_geometries([pcd_neighbor_set, core_from_target],
-                                          window_name='before filtered')
-
         signed_dis = trimesh.proximity.signed_distance(meshA, points_in_ref[idx])
 
         idx_inner = np.argwhere(signed_dis > 0).flatten().tolist()
         neighbor_set_inner = points_in_ref[idx[idx_inner]]
 
         filt_nebor_cent = np.mean(neighbor_set_inner, axis=0)
-        neighbor_set_centroid = o3d.geometry.TriangleMesh.create_sphere(radius=1.0).translate(
-            (filt_nebor_cent[0], filt_nebor_cent[1], filt_nebor_cent[2]))
-
-        pcd_neighbor_set_inner = o3d.geometry.PointCloud()
-        pcd_neighbor_set_inner.points = o3d.utility.Vector3dVector(neighbor_set_inner)
-
-        o3d.visualization.draw_geometries([pcd_neighbor_set_inner, core_from_target, neighbor_set_centroid],
-                                          window_name='filtered')
 
         filtered_dis = np.array(dis_tree[idx_inner])*1000
         gaussian_weight = np.array(gaussian_dis(filtered_dis, sigma=7, mu=np.min(signed_dis[idx_inner])))
@@ -259,9 +214,9 @@ for j in np.arange(4068, 4078):
         print("高斯加权平均后误差", average_dis, "mm")
         print("直接平均值误差", np.average(filtered_dis), "mm")
         print("点到质心的距离 ", dis_to_cent)
-        print("临近点数量", len(dis_tree))
+        print("过滤前邻近点数量", len(dis_tree))
+        print("过滤后邻近点数量", len(neighbor_set_inner))
 
-        plotter.show()
         del meshA, meshB
         gc.collect()
 
