5ff09a0fb88cc22366d2143a033580735f7d740f
geoff <geoffrey.1500@outlook.com>
2022-04-18 15:47:12 +0800

    相机姿态基本校准完成，可以考虑加入镜头畸变所带来的影响了

--- test.py
+++ test.py
@@ -195,8 +195,8 @@ def my_ray_casting2():
     data = pd.read_csv("data/UAV_only.csv")
     print(data.head(5))
     cam_loc = data[["x", "y", "z"]].values
-    euler_ang = data[["heading", "pitch", "roll"]].values * np.array([[-1, 1, 1]]) + np.array([[0, 0, 0]])
-    rot_mat_set = R.from_euler('ZXY', euler_ang, degrees=True)
+    euler_ang = data[["roll", "pitch", "heading"]].values * np.array([[1, 1, -1]])
+    rot_mat_set = R.from_euler('yxz', euler_ang, degrees=True)
 
     mesh = o3d.io.read_triangle_mesh('data/UAV_only.ply')
 
@@ -217,12 +217,12 @@ def my_ray_casting2():
     scene = o3d.t.geometry.RaycastingScene()
     scene.add_triangles(mesh_for_ray)
 
-    kkk = 28
+    kkk = 220
     print(cam_loc[kkk].reshape((-1, 1)))
     ex_mat = np.vstack((np.hstack((rot_mat_set.as_matrix()[kkk], cam_loc[kkk].reshape((-1, 1)))), [[0, 0, 0, 1]]))
-    ex_mat = np.vstack((np.hstack(([[1, 0, 0],
-                                    [0, 1, 0],
-                                    [0, 0, -1]], cam_loc[kkk].reshape((-1, 1)))), [[0, 0, 0, 1]]))
+    # ex_mat = np.vstack((np.hstack(([[1, 0, 0],
+    #                                 [0, 1, 0],
+    #                                 [0, 0, -1]], cam_loc[kkk].reshape((-1, 1)))), [[0, 0, 0, 1]]))
     # print(ex_mat)
     rays = o3d.t.geometry.RaycastingScene.create_rays_pinhole(
         intrinsic_matrix=intrinsic_matrix,
@@ -231,16 +231,16 @@ def my_ray_casting2():
         height_px=int(resol_y),
     )
 
-    rays_direction = np.asarray(rot_mat_set[kkk].apply(sensor_plane_point()))
-    print(rays_direction)
-    rays_direction = rays_direction / rays_direction.max(axis=1).reshape((-1, 1))
-
-    rays_starts = np.expand_dims(cam_loc[kkk], 0).repeat(len(rays_direction), axis=0)
-
-    rays_sets = np.hstack((rays_starts, rays_direction))
-    print(rays_sets)
-    rays = o3d.core.Tensor(rays_sets,
-                           dtype=o3d.core.Dtype.Float32)
+    # rays_direction = np.asarray(rot_mat_set[kkk].apply(sensor_plane_point(points_per_side_=1824)))
+    # print(rays_direction)
+    # rays_direction = rays_direction / rays_direction.max(axis=1).reshape((-1, 1))
+    #
+    # rays_starts = np.expand_dims(cam_loc[kkk], 0).repeat(len(rays_direction), axis=0)
+    #
+    # rays_sets = np.hstack((rays_starts, rays_direction))
+    # print(rays_sets)
+    # rays = o3d.core.Tensor(rays_sets,
+    #                        dtype=o3d.core.Dtype.Float32)
 
     # We can directly pass the rays tensor to the cast_rays function.
     ans = scene.cast_rays(rays)
@@ -294,5 +294,126 @@ def my_ray_casting2():
     o3d.visualization.draw_geometries([pcd])
 
 
+def visualize_camera():
+    data = pd.read_csv("data/UAV_only.csv")
+    print(data.head(5))
+    cam_loc = data[["x", "y", "z"]].values
+    euler_ang = data[["roll", "pitch", "heading"]].values * np.array([[1, 1, -1]])
+    rot_mat_set = R.from_euler('yxz', euler_ang, degrees=True)
+
+    kkk = 252
+    print(euler_ang[kkk])
+    print(rot_mat_set[kkk].as_matrix())
+    blue = np.array([0, 0, 1])
+    points_tar = np.asarray(rot_mat_set[kkk].apply(sensor_plane_point(points_per_side_=30)))
+    colors_tar = np.expand_dims(blue, 0).repeat(len(points_tar), axis=0)
+    pcd_tar = o3d.geometry.PointCloud()
+    pcd_tar.points = o3d.utility.Vector3dVector(points_tar)
+    pcd_tar.colors = o3d.utility.Vector3dVector(colors_tar)
+
+    red = np.array([1, 0, 0])
+    rot_mat_ref = R.from_euler('yxz', [[0, 0, euler_ang[kkk, -1]]], degrees=True)
+    points_ref = np.asarray(rot_mat_ref[0].apply(sensor_plane_point(points_per_side_=30)))
+    colors_ref = np.expand_dims(red, 0).repeat(len(points_ref), axis=0)
+    pcd_ref = o3d.geometry.PointCloud()
+    pcd_ref.points = o3d.utility.Vector3dVector(points_ref)
+    pcd_ref.colors = o3d.utility.Vector3dVector(colors_ref)
+
+    green = np.array([0, 1, 0])
+    rot_mat_ref_2 = R.from_euler('yxz', [[0, 0, 0]], degrees=True)
+    points_ref_2 = np.asarray(rot_mat_ref_2[0].apply(sensor_plane_point(points_per_side_=30)))
+    colors_ref_2 = np.expand_dims(green, 0).repeat(len(points_ref_2), axis=0)
+    pcd_ref_2 = o3d.geometry.PointCloud()
+    pcd_ref_2.points = o3d.utility.Vector3dVector(points_ref_2)
+    pcd_ref_2.colors = o3d.utility.Vector3dVector(colors_ref_2)
+
+    o3d.visualization.draw_geometries([pcd_ref, pcd_tar, pcd_ref_2])
+
+
+def my_ray_casting3():
+    data = pd.read_csv("data/UAV_only.csv")
+    print(data.head(5))
+    cam_loc = data[["x", "y", "z"]].values
+    euler_ang = data[["roll", "pitch", "heading"]].values * np.array([[1, 1, -1]])
+    rot_mat_set = R.from_euler('yxz', euler_ang, degrees=True)
+
+    mesh = o3d.io.read_triangle_mesh('data/UAV_only.ply')
+
+    original_data = pd.read_csv('data/UAV_only.xyz', header=None, sep=' ').to_numpy()
+    points = original_data[:, 0:3]
+    colors = original_data[:, 6::] / 255
+    normals = original_data[:, 3:6]
+
+    pcd = o3d.geometry.PointCloud()
+    pcd.points = o3d.utility.Vector3dVector(points)
+    pcd.colors = o3d.utility.Vector3dVector(colors)
+    pcd.normals = o3d.utility.Vector3dVector(normals)
+
+    mesh.vertex_normals = o3d.utility.Vector3dVector(normals)
+    # o3d.visualization.draw_geometries([pcd, mesh], mesh_show_wireframe=True, point_show_normal=False)
+
+    mesh_for_ray = o3d.t.geometry.TriangleMesh.from_legacy(mesh)
+    scene = o3d.t.geometry.RaycastingScene()
+    scene.add_triangles(mesh_for_ray)
+
+    kkk = 220
+    print(cam_loc[kkk].reshape((-1, 1)))
+
+    rays_direction = np.asarray(rot_mat_set[kkk].apply(sensor_plane_point(points_per_side_=1824)))
+    print(rays_direction)
+    rays_direction = rays_direction / rays_direction.max(axis=1).reshape((-1, 1))
+
+    rays_starts = np.expand_dims(cam_loc[kkk], 0).repeat(len(rays_direction), axis=0)
+
+    rays_sets = np.hstack((rays_starts, rays_direction))
+    print(rays_sets)
+    rays = o3d.core.Tensor(rays_sets,
+                           dtype=o3d.core.Dtype.Float32)
+
+    # We can directly pass the rays tensor to the cast_rays function.
+    ans = scene.cast_rays(rays)
+
+    original_triangle = np.asarray(mesh.triangles)
+    # print(original_triangle)
+    # 在 geometry_ids 和 primitive_ids 中 4294967295 等同于 finite 无效数字
+
+    triangle_index = ans['primitive_ids'][ans['primitive_ids'] != 4294967295].numpy()
+    hit_triangles = original_triangle[triangle_index]
+    tri_pts_cors_1 = np.asarray(mesh.vertices)[hit_triangles[:, 0]]
+    tri_pts_cors_2 = np.asarray(mesh.vertices)[hit_triangles[:, 1]]
+    tri_pts_cors_3 = np.asarray(mesh.vertices)[hit_triangles[:, 2]]
+
+    print(tri_pts_cors_1)
+
+    rays_index = ans['t_hit'].isfinite()
+    inter_pts_cors = rays[rays_index][:, :3] + rays[rays_index][:, 3:] * ans['t_hit'][rays_index].reshape((-1, 1))
+    inter_pts_cors = inter_pts_cors.numpy()
+    print(inter_pts_cors)
+
+    print(len(inter_pts_cors), len(triangle_index))
+
+    dis_pts2tri_1 = np.sum(np.abs(inter_pts_cors - tri_pts_cors_1), axis=1).reshape(-1, 1)
+    dis_pts2tri_2 = np.sum(np.abs(inter_pts_cors - tri_pts_cors_2), axis=1).reshape(-1, 1)
+    dis_pts2tri_3 = np.sum(np.abs(inter_pts_cors - tri_pts_cors_3), axis=1).reshape(-1, 1)
+
+    dis_pts2tri_set = np.hstack((dis_pts2tri_1, dis_pts2tri_2, dis_pts2tri_3))
+    print(dis_pts2tri_set)
+
+    index_xx = np.argmin(dis_pts2tri_set, axis=1)
+    print(index_xx)
+
+    index_ff = hit_triangles[np.arange(len(hit_triangles)), index_xx]
+    print(index_ff)
+    print(hit_triangles)
+
+    # print(np.sum(np.abs(inter_pts_cors - tri_pts_cors), axis=1))
+
+    pcd = o3d.geometry.PointCloud()
+    pcd.points = o3d.utility.Vector3dVector(points[index_ff])
+    pcd.colors = o3d.utility.Vector3dVector(colors[index_ff])
+    # pcd.normals = o3d.utility.Vector3dVector(normals[ans['primitive_ids'].numpy()])
+    o3d.visualization.draw_geometries([pcd])
+
+
 if __name__ == '__main__':
-    my_ray_casting2()
+    my_ray_casting3()
